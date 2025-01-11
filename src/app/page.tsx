3cac5bc15adef6bd736e9844f1f82e5e490b14cd
Nkanyiso Ncube <72646420+nkanyiso-mrnlabs@users.noreply.github.com>
2025-01-11 21:16:38 +0200

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Button>Hello</Button>
+      <Button variant="teritary">Hello</Button>
       <Button variant="destructive">Hello</Button>
+      <Button variant="muted">Hello</Button>
+      <Button variant="outline">Hello</Button>
+
+      <Button variant="primary">Hello</Button>
       <p className="text-balance text-3xl sm:text-4xl">Hello</p>
     </div>
   );
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,26 +9,28 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
+        primary:
           "bg-gradient-to-b from-blue-600 to-blue-700 text-primary-foreground hover:from-blue-700 hover:to-blue-700",
         destructive:
           "bg-gradient-to-b from-amber-600 to-amber-700 text-destructive-foreground hover:from-amber-700 hover:to-amber-700",
         outline:
-          "",
+          "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
-          "",
-        ghost: "",
-        link: "",
+          "bg-white text-black hover:bg-neutral-100 ",
+        ghost: "border-transparent shadow-none hover:bg-accent hover:text-accent-foreground",
+        muted: "bg-neutral-200 text-neutral-600 hover:bg-neutral-200/80",
+        teritary: "bg-blue-100  text-blue-600 border-transparent hover:bg-blue-200 shadow-none"
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
-      },
+        default: "h-10 px-4 py-2",
+        sm: "h-8 rounded-md px-3",
+        xs: "h-7 rounded-md px-2 text-xs",
+        lg: "h-12 rounded-md px-8",
+        icon: "h-8 w-8",
+        },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "primary",
       size: "default",
     },
   }
