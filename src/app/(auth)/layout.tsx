9495b11da9cc2a482794cd252999b4e66fbefac7
Nkanyiso Ncube <72646420+nkanyiso-mrnlabs@users.noreply.github.com>
2025-01-12 00:58:47 +0200

    Update layout.tsx

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,23 +1,23 @@
-import Image from 'next/image'
-import React from 'react'
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import React from "react";
 interface AuthLayoutProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
-const AuthLayout = ( { children }: AuthLayoutProps) => {
+const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
     <main className="bg-neutral-100 min-h-screen">
-        <div className="mx-auto mx-w-screen-2xl p-4">
-            <nav className="flex justify-between items-center">
-                <div className="flex items-center gap-2">
-                    <Image src="/logo.svg" alt="Logo" width={100} height={100} />
-                    
-                </div>
-            </nav>
+      <div className="mx-auto mx-w-screen-2xl p-4">
+        <nav className="flex justify-between items-center">
+          <Image src="/logo.svg" alt="Logo" width={100} height={100} />
+          <Button variant="secondary">Sign In</Button>
+        </nav>
+       <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
         {children}
         </div>
-     
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
